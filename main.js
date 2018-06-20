221419e9b712e04c653924b3a40bb261991a159d
Andreas Opferkuch <s-h-a-d-o-w@users.noreply.github.com>
2018-06-20 17:30:19 +0200

    Preloading of Montserrat font and ffmpeg (where supported)

--- main.js
+++ main.js
@@ -8,7 +8,17 @@ const port = process.env.PORT || 3000
 
 console.log('Using production bundle');
 app.use(compression());
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist', {
+	setHeaders: function (res, path, stat) {
+		if(path.indexOf('index.html') > 0) {
+			// Maybe use server push via cloud flare in the future:
+			// https://www.cloudflare.com/website-optimization/http2/serverpush/
+			// Especially since the following works with Chrome but not Firefox
+			res.set('Link', '<fonts/Montserrat-Light.woff>; rel=preload; as=font, <ffmpeg-worker-webm.js>; rel=preload; as=script');
+		}
+	}
+}));
 
 app.listen(port, function () {
 	console.log(`Listening on port ${port}!\n`);
